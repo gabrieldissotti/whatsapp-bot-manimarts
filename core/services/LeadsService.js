a88c27e64f70fe523a9294c934eb1abb6b0235cc
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2022-11-14 03:37:26 -0300

    refactor(logs): removed logs used to test flow in vercel cloud

--- core/services/LeadsService.js
+++ core/services/LeadsService.js
@@ -14,71 +14,37 @@ class LeadsService {
       if (!recipientPhoneNumber)
         throw new Error('phone number is not present in webhook payload');
 
-      console.log(
-        `eadsService: {phone: ${recipientPhoneNumber}} |recipient phone number ok`
-      );
-
       let lead = await this.leadsRepository.getLead(recipientPhoneNumber);
       let isFirstContact = false;
       if (!lead) {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | it's the first contact`
-        );
         isFirstContact = true;
         lead = this.leadsRepository.createLead({
           phoneNumber: recipientPhoneNumber,
           name: 'Unnamed yet',
           stagePosition: 0,
         });
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | lead created successfully`
-        );
       }
 
       const scriptStages = await this.scriptsRepository.getStages();
-      console.log(
-        `LeadsService: {phone: ${recipientPhoneNumber}} | script stages ok | count: ${scriptStages.length}`
-      );
       if (!scriptStages) {
         throw new Error('no script found in database to get stages');
       }
 
       let nextStage = scriptStages[0];
       if (!isFirstContact) {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | it's the ${
-            lead.stage_position + 1
-          }º contact`
-        );
         nextStage = scriptStages.find(
           (stage) => stage.position === lead.stage_position + 1
         );
       }
 
-      console.log(
-        `LeadsService: {phone: ${recipientPhoneNumber}} | actual stage ${JSON.stringify(
-          nextStage
-        )}º`
-      );
-
       if (nextStage?.message?.template) {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | sending first text message`
-        );
         await this.whatsAppBusinessCloudAPI.sendMessageByTemplate(
           nextStage.message.template,
           recipientPhoneNumber
         );
-      } else {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | stage does not have message by template, skipping`
-        );
       }
 
       nextStage?.message?.medias?.map(async ({ type, url }) => {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | sending media in ${nextStage.position}º stage | {media: ${url}}`
-        );
         switch (type) {
           case 'audio':
             await this.whatsAppBusinessCloudAPI.sendAudioMessage(
@@ -91,11 +57,6 @@ class LeadsService {
             break;
         }
       });
-      if (!nextStage?.message?.medias?.length) {
-        console.log(
-          `LeadsService: {phone: ${recipientPhoneNumber}} | stage does not have any media message, skipping`
-        );
-      }
 
       await this.leadsRepository.updateLead({
         phoneNumber: recipientPhoneNumber,
